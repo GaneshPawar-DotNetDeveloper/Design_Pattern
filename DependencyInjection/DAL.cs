f3077c34062e3a088d456769a26997f46cb8e624
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-07-17 01:25:20 +0530

    Add project files.

---
+++ DependencyInjection/DAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+  public  class DAL :IDAL
+    {
+        public void Insert()
+        {
+            // ado.net code
+            Console.WriteLine("Data Inserted successfully");    
+        }
+
+    }
+}
---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace DependencyInjection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* BLL b = new BLL(new DAL());
+            b.Insert();*/
+
+
+            UnityContainer container = new UnityContainer();
+            container.RegisterType<IDAL, DAL>();
+
+            BLL b = container.Resolve<BLL>();
+            b.Insert();
+                
+            Console.ReadLine();
+
+
+        }
+    }
+}
